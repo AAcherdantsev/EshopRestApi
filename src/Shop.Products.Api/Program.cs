6a2bea23464de1c19d0e8eaa7e72b777d45deecd
AAcherdantsev <75489700+AAcherdantsev@users.noreply.github.com>
2025-10-04 21:41:41 +0200

    Add API versioning

--- src/Shop.Products.Api/Program.cs
+++ src/Shop.Products.Api/Program.cs
@@ -12,14 +12,37 @@ builder.Services.SwaggerDocument(o =>
 {
     o.DocumentSettings = s =>
     {
+        s.DocumentName = "v1";
+        s.Title = "Products API V1";
+        s.Version = "v1";
         s.SchemaSettings.SchemaNameGenerator = new DefaultSchemaNameGenerator();
     };
+    o.MaxEndpointVersion = 1;
+    o.MinEndpointVersion = 1;
+});
+
+builder.Services.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.DocumentName = "v2";
+        s.Title = "Products API V2";
+        s.Version = "v2";
+        s.SchemaSettings.SchemaNameGenerator = new DefaultSchemaNameGenerator();
+    };
+    
+    o.MaxEndpointVersion = 2;
+    o.MinEndpointVersion = 2;
 });
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-app.UseFastEndpoints();
+app.UseFastEndpoints(c =>
+{
+    c.Versioning.Prefix = "v";
+    c.Versioning.PrependToRoute = true;
+});
 app.UseSwaggerGen();
 
 app.Run();
